6bc61aea43a1721c936e6b177b39e705fbe5ccb4
Nadir Mansouri <61685527+nadir9292@users.noreply.github.com>
2024-06-28 11:28:01 +0200

    try fix babel issue for CI/CD 2

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,12 @@
 export default [
   {
     files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2021
+    },
     rules: {
-      semi: ['error', 'never'],
-      quotes: ['error', 'single'],
+      'semi': ['error', 'never'],
+      'quotes': ['error', 'single'],
     },
   },
 ]
